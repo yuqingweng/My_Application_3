60ad6829bf75240344cf1d29330b58cbd59cae93
wengyuqing <377928782@qq.com>
2020-07-17 20:35:20 +0800

    fail_3

---
+++ app/src/main/java/com/example/my_application_3/Fruit.java
@@ -0,0 +1,39 @@
+package com.example.my_application_3;
+
+public class Fruit {
+
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId){
+        this.name = name;
+        this.imageId = imageId;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
